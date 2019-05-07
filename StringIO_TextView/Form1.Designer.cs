5482b2767644b3ddf9b76e437a3f347825ee742d
Yura Choi <s2017i19@e-mirim.hs.kr>
2019-05-07 15:57:42 +0900

    #2 버튼을 눌렀을 때 사용자 입력을 라벨에 출력

--- StringIO_TextView/Form1.Designer.cs
+++ StringIO_TextView/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.txtEdit = new System.Windows.Forms.TextBox();
             this.btnEdit = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtEdit
@@ -49,29 +49,31 @@
             this.btnEdit.TabIndex = 1;
             this.btnEdit.Text = "입력";
             this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
-            // label1
+            // lblResult
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("굴림", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(12, 130);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 19);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "결과 : ";
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("굴림", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblResult.Location = new System.Drawing.Point(12, 130);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(65, 19);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "결과 : ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(236, 200);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResult);
             this.Controls.Add(this.btnEdit);
             this.Controls.Add(this.txtEdit);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,7 +83,7 @@
 
         private System.Windows.Forms.TextBox txtEdit;
         private System.Windows.Forms.Button btnEdit;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResult;
     }
 }
 
--- StringIO_TextView/Form1.cs
+++ StringIO_TextView/Form1.cs
@@ -9,11 +9,23 @@ using System.Windows.Forms;
 
 namespace StringIO_TextView
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form //partial => 클래스를 쪼개놓았다고 생각(나중에 하나의 클래스로 합쳐서 봄)
     {
+        string OrgStr = ""; // "결과 : " 문자열 저장  //org => original
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OrgStr = this.lblResult.Text;
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            this.lblResult.Text = this.OrgStr + this.txtEdit.Text;
         }
     }
 }
